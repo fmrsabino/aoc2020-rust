c1dd409e7437408dce244cd80cc302f1116d5a23
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-03-03 23:39:52 +0100

    Day 4: Add year range

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,20 +1,39 @@
 use std::collections::HashMap;
+use std::fs;
+use std::str::FromStr;
 
 use regex::Regex;
-use std::fs::File;
-use std::io::{BufReader, BufRead};
-use std::fs;
-use std::error::Error;
 
 struct Passport {
-    _birth_year: String,
-    _issue_year: String,
-    _expiration_year: String,
+    _birth_year: Year,
+    _issue_year: Year,
+    _expiration_year: Year,
     _height: String,
     _hair_color: String,
     _eye_color: String,
     _passport_id: String,
     _country_id: Option<String>,
+}
+
+struct YearRange {
+    value: (usize, usize),
+}
+
+impl YearRange {
+    fn new(range: (usize, usize)) -> Option<Self> {
+        return if range.0 < range.1 { Some(YearRange { value: range }) } else { None };
+    }
+}
+
+struct Year {
+    _value: usize,
+}
+
+impl Year {
+    fn new(value: &str, range: YearRange) -> Option<Self> {
+        let v = usize::from_str(value).ok()?;
+        return if v >= range.value.0 && v <= range.value.1 { Some(Year { _value: v }) } else { None };
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
@@ -43,9 +62,9 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
 fn build_passport(raw_data: &HashMap<&str, &str>) -> Option<Passport> {
     Some(
         Passport {
-            _birth_year: raw_data.get("byr")?.to_string(),
-            _issue_year: raw_data.get("iyr")?.to_string(),
-            _expiration_year: raw_data.get("eyr")?.to_string(),
+            _birth_year: Year::new(raw_data.get("byr")?, YearRange::new((1920, 2002))?)?,
+            _issue_year: Year::new(raw_data.get("iyr")?, YearRange::new((2010, 2020))?)?,
+            _expiration_year: Year::new(raw_data.get("eyr")?, YearRange::new((2020, 2030))?)?,
             _height: raw_data.get("hgt")?.to_string(),
             _hair_color: raw_data.get("hcl")?.to_string(),
             _eye_color: raw_data.get("ecl")?.to_string(),
