e743d3bba38a9ee2e31d7179cb9baf3937e5126a
fmrsabino <3332770+fmrsabino@users.noreply.github.com>
2021-03-15 16:01:05 +0100

    Add Day 6 Part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,10 +1,12 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fs;
+
+use itertools::Itertools;
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let input = fs::read_to_string("res/input.txt")?;
 
-    let sum: usize = input.split("\n\n")
+    let sum1: usize = input.split("\n\n")
         .map(|group|
             group.chars()
                 .filter(|c| !c.is_ascii_whitespace())
@@ -13,6 +15,19 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
         )
         .sum();
 
-    println!("Count of sum: {}", sum);
+    let sum2 = input.split("\n\n")
+        .map(|group| {
+            let counts: HashMap<char, usize> = group.chars().counts();
+            let required = group.lines().count();
+
+            counts.iter()
+                .filter(|&(k, v)| *k != '\n' && *v >= required)
+                .count()
+        })
+        .sum::<usize>();
+
+
+    println!("Part 1 count: {}", sum1);
+    println!("Part 2 count: {}", sum2);
     Ok(())
 }
